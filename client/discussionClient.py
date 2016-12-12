e781e82b4db2703640ea7b74045e9e5dd809cb3b
James Morris <james.a.morris@stonybrook.edu>
2016-12-12 16:04:38 -0500

    Potential Fix for RG r

--- client/discussionClient.py
+++ client/discussionClient.py
@@ -269,13 +269,14 @@ def main():
 
                         if(pattern_1.match(usr_input[1])):
                             locate = int(usr_input[1])
-                            message.update({"postSubject":sort_group[locate-1]["sub"]["name"]})
+                            message.update({"postSubject":sort_group[locate-1]["name"]})
+                            message.update({"postNumber":sort_group[locate-1]["cont"]["postNumber"]
   
                         elif(pattern_2.match(usr_input[1])):
                             split = usr_input[1].split("-")
                             for locate in range(int(split[0]), int(split[1])+1):
-                                message.update({"postSubject":sort_group[locate-1]["sub"]["name"]})
-                        message.update({"postNumber":1})
+                                message.update({"postSubject":sort_group[locate-1]["name"]})
+                                message.update({"postNumber":sort_group[locate-1]["cont"]["postNumber"]                            
                         
                         updatevalue(usr_nm, CURRENT_READ, sort_group[locate-1]["sub"])
 
